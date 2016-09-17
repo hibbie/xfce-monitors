e608c853a29c54d04380e4b54c86baaf3b588017
Thomas Hibbard <hibbie@mac.com>
2016-09-17 00:41:01 -0500

    .

--- case_temp.sh
+++ case_temp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CASET=$(sensors | grep '(high = +70.0°C, hyst = +68.0°C)' | awk -v RS="" '{print ($13-$2)}' | sed -e 's/[^0-9]//g')
 CASEL='env'
-if [ $(echo $CASET'<34' | bc) == 1 ]; then
+if [ $(echo $CASET'<50' | bc) == 1 ]; then
     echo "<txt><span font='10' fgcolor='#fff'>$CASEL: </span><span weight='Bold' fgcolor='#00B000' font='9.2' letter_spacing='2000' >$CASET°</span> </txt>"
 else
-    if [ $(echo $CASET'<36' | bc) == 1 ]; then
+    if [ $(echo $CASET'<60' | bc) == 1 ]; then
        echo "<txt><span font='10' fgcolor='#fff'>$CASEL: </span><span weight='Bold' font='9.2' fgcolor='Yellow'>$CASET°</span> </txt>"
     else
        echo "<txt><span font='10' fgcolor='#fff'>$CASEL: </span><span weight='Bold' font='9.2' fgcolor='Red' bgcolor='Yellow'> $CASET° </span> </txt>"
